11c8de86853f534a7f1e666cede5b8869a35d673
softstream-link <softstream.link@gmail.com>
2024-01-31 15:36:07 -0500

    settable sender

--- tests/links_connect/callbacks/decorator/driver_test.py
+++ tests/links_connect/callbacks/decorator/driver_test.py
@@ -49,60 +49,45 @@ class ExampleCallback2(DecoratorDriver):
         self._one_recv += 1
 
 
+def test_callback_settable_sender_import():
+    from links_connect.callbacks import SettableSender
+    from links_connect.callbacks._04_decorator.driver import Sender
+    clbk = ExampleCallback1()
+    assert isinstance(clbk, SettableSender)
+    assert isinstance(clbk.sender , Sender)
+
+
 def test_callback_driver():
     clbk1 = ExampleCallback1() + LoggerCallback()
     clbk2 = ExampleCallback2() + LoggerCallback()
 
     log.info(f"clbk: {clbk1}")
     log.info(f"clbk: {clbk2}")
-    assert (
-        CallbackRegistry.get(
-            clbk1.__class__.__name__
-        ).on_recv_filter_callback_entries.len()
-        == 2
-    )
-    assert (
-        CallbackRegistry.get(
-            clbk1.__class__.__name__
-        ).on_sent_filter_callback_entries.len()
-        == 2
-    )
-    assert (
-        CallbackRegistry.get(
-            clbk2.__class__.__name__
-        ).on_recv_filter_callback_entries.len()
-        == 1
-    )
-    assert (
-        CallbackRegistry.get(
-            clbk2.__class__.__name__
-        ).on_sent_filter_callback_entries.len()
-        == 0
-    )
+    assert CallbackRegistry.get(clbk1.__class__.__name__).on_recv_filter_callback_entries.len() == 2
+    assert CallbackRegistry.get(clbk1.__class__.__name__).on_sent_filter_callback_entries.len() == 2
+    assert CallbackRegistry.get(clbk2.__class__.__name__).on_recv_filter_callback_entries.len() == 1
+    assert CallbackRegistry.get(clbk2.__class__.__name__).on_sent_filter_callback_entries.len() == 0
 
     clbk1.on_recv(ConId(), {"one_recv": {}, "two_recv": {}, "three_recv": {}})
     clbk2.on_recv(ConId(), {"one_recv": {}, "two_recv": {}, "three_recv": {}})
     clbk1.on_sent(ConId(), {"one_sent": {}, "two_sent": {}, "three_sent": {}})
-    assert (
-        clbk1._one_recv == 1
-    )  # called because it is the first @on_recv annotated method
+    assert clbk1._one_recv == 1  # called because it is the first @on_recv annotated method
     assert clbk1._two_recv == 0
     assert clbk1._three_recv == 0
-    assert (
-        clbk1._one_sent == 1
-    )  # called because it is the first @on_recv annotated method
+    assert clbk1._one_sent == 1  # called because it is the first @on_recv annotated method
     assert clbk1._two_sent == 0
     assert clbk1._three_sent == 0
-    assert (
-        clbk2._one_recv == 1
-    )  # called because it is the first @on_recv annotated method
+    assert clbk2._one_recv == 1  # called because it is the first @on_recv annotated method
 
+def main():
+    test_callback_settable_sender_import()
+    test_callback_driver()
 
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.DEBUG,
         format="%(asctime)s [%(levelname)8s] (%(threadName)10s) %(message)s (%(filename)s:%(lineno)s)",
     )
-    test_callback_driver()
+    main()
     # import pytest
     # pytest.main([__file__])  # fails because ExampleCallback get loaded twice which causes callbacks to be registered twice
